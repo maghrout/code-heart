54961aca537cade4d30af75fc8d449b510b457dc
Alex Clapperton <hi@alexclapperton.co.uk>
2020-02-02 15:00:52 +0000

    ✨ Add data

---
+++ foo.js
@@ -0,0 +1,486 @@
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Alex Clapperton',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U7P15BX4G-9b36386dc3d7-512',
+        buttonText: 'More about Alex',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Moujib Aghrout',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U1QH8PKEV-263a9101ec63-512',
+        buttonText: 'More about Moujib',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Gina Manns',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UMMR02LBG-b297a879185d-512',
+        buttonText: 'More about Gina',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Adam Pendlebury',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0LC7E91U-75d1e321b300-512',
+        buttonText: 'More about Adam',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Beth Haliwell',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UC35KDWLT-596844509fc7-512',
+        buttonText: 'More about Beth',
+          role: 'Delivery',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Louise Knowles',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U1EG32CGJ-97decf02e412-512',
+        buttonText: 'More about Louise',
+          role: 'QA',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Salma Alam-Naylor',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UKXU9T34N-703b3655853d-512',
+        buttonText: 'More about Salma',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Amanda Ohrn',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UC78X0NGK-8b1171e1351e-512',
+        buttonText: 'More about Amanda',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Mircea Ungureanu',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UF83SHT9N-0360c3998e8d-512',
+        buttonText: 'More about Mircea',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Will Tarpey',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U9NB97TJQ-54f31a5833d6-512',
+        buttonText: 'More about Will',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Ross Enston',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U9Q6NHKBR-16aaac68cd7e-512',
+        buttonText: 'More about Ross',
+          role: 'Conversion',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Siu Wong',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U9PRFP85C-39721fe76793-512',
+        buttonText: 'More about Siu',
+          role: 'Conversion',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Jo Latham',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U6XUAL08L-d997f4969da7-512',
+        buttonText: 'More about Jo',
+          role: 'Delivery',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Barney Scott',
+      image: 'https://pbs.twimg.com/profile_images/2412290818/2n9tkfnl3q6mof4i33zi_400x400.jpeg',
+        buttonText: 'More about Barney',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Mark Owen',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0E5DUFKL-2bc57bffb44e-512',
+        buttonText: 'More about Mark',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Chris Forsyth',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U4029NPGE-f6742636c18a-512',
+        buttonText: 'More about Chris',
+          role: 'QA',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Claire Kerley',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U3PHFJ7LN-d6abf66ea997-512',
+        buttonText: 'More about Claire',
+          role: 'Delivrey',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Alan Ionita',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UM89SNJ30-2852c8039b58-512',
+        buttonText: 'More about Alan',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Amy Gillett',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UNEQJR98V-7446af77ba1f-512',
+        buttonText: 'More about Amy',
+          role: 'Conversion',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Joel Stein',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U03M26YNC-939e2cd69e6c-512',
+        buttonText: 'More about Joel',
+          role: 'Content',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Mike Banner',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UGYSQMLAG-800bd8d39d40-512',
+        buttonText: 'More about Developer',
+          role: '',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Toby Burkill',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U7RD8S42U-6a420f669b87-512',
+        buttonText: 'More about Toby',
+          role: '',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Kate Reidy',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U50SKGLSH-e8f23e3a7135-512',
+        buttonText: 'More about Kate',
+          role: '',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Colin Preston',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0GM5LGBU-d5081e191933-512',
+        buttonText: 'More about Colin',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Ken Fulton',
+      image: 'https://pbs.twimg.com/profile_images/861860303759441920/_iJjbZyn_400x400.jpg',
+        buttonText: 'More about Ken',
+          role: 'QA',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Mark Wilson',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U9MNQGVPT-e0117b285c15-512',
+        buttonText: 'More about Mark',
+          role: 'QA',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Ellice Weszka',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UKS3KEGGZ-b596cdf3d4ec-512',
+        buttonText: 'More about Ellice',
+          role: '',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Sean White',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UA2MEVA7J-3d68f298f2f7-512',
+        buttonText: 'More about Sean',
+          role: 'Delivery',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Dan Carr',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UC7KXP75K-09b6d10cce78-512',
+        buttonText: 'More about Dan',
+          role: 'Delivery',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Jamie Greenwood',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UGYTTHX3L-17099d536b15-512',
+        buttonText: 'More about Jamie',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+
+{
+  frontFace: {
+    name: 'Jess Sherratt',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U8FQ3R401-8a2467f2b8a3-512',
+        buttonText: 'More about Jess',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Wini Tse',
+      image: 'https://i.pinimg.com/originals/56/44/0f/56440f97828d99cf47d25d040aea5a88.jpg',
+        buttonText: 'More about Wini',
+          role: 'Founder',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Tony Foggett',
+      image: 'https://pbs.twimg.com/profile_images/704625654621409281/aJwYAs70_400x400.jpg',
+        buttonText: 'More about Tony',
+          role: 'Founder',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Louis Georgiou',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0H2X417E-57d150841116-512',
+        buttonText: 'More about Louis',
+          role: 'Founder',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Jools Chester',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0G856K6F-0ce13dd5d74c-512',
+        buttonText: 'More about Jools',
+          role: 'IT',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Claire Freeman',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U1QHEJHA8-95349a82097d-512',
+        buttonText: 'More about Claire',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Diana Goodwin',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0UAH348J-59a03f7a05d6-512',
+        buttonText: 'More about Diana',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Adam Hopwood',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U3LLHPBMH-041a0e9906b6-512',
+        buttonText: 'More about Adam',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Michelle May',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UALB55JJH-048b1d61fe38-512',
+        buttonText: 'More about Designer',
+          role: 'Designer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Charles Kelly',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UGYSZGZUL-ad57465f6e29-512',
+        buttonText: 'More about Charles',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Sami Anderson',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UHUS00E56-101d0bb9f512-512',
+        buttonText: 'More about Sami',
+          role: 'Content',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Dan Tse',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U23H85B4K-ded7da0a1957-512',
+        buttonText: 'More about Dan',
+          role: 'Conversion',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Duncan Bloor',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U6MG62STS-ab4e7681eff2-512',
+        buttonText: 'More about Duncan',
+          role: 'Content',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Dave Heath',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U0GAFPVCL-dd8506f60ae8-512',
+        buttonText: 'More about Dave',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Emma Haslam',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UFXBY5YRY-642ff3657ca7-512',
+        buttonText: 'More about Emma',
+          role: 'Delivery',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Tracey Kerr',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UGYSLL1V2-7095122e4158-512',
+        buttonText: 'More about Tracey',
+          role: 'QA',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Matthew Hughes',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-U6RS09BEU-a6974a3d0a5c-512',
+        buttonText: 'More about Matthew',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{
+  frontFace: {
+    name: 'Marcel Legane',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UNDKP36US-b2f07f0a909c-512',
+        buttonText: 'More about Marcel',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{
+  frontFace: {
+    name: 'Daniel Lane',
+      image: 'https://ca.slack-edge.com/T0315G9HQ-UM86FTFMZ-b8d2ad101cb6-512',
+        buttonText: 'More about Daniel',
+          role: 'Developer',
+        },
+  backFace: { },
+},
+{ hidden: true },
+{ hidden: true },
+{ hidden: true },
+{ hidden: true }
